ab832faaa68f073edf39b625efff9e552d708099
ULTIMATEFIGHTEER <136517540+ULTIMATEFIGHTEER@users.noreply.github.com>
2026-08-20 13:37:47 -0700

    Initial commit

---
+++ Lyrics.java
@@ -0,0 +1,18 @@
+
+/**
+ * Prints out lyrics.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lyrics
+{
+
+    /**
+     * Prints out lyrics.
+     * @param args Not used
+     **/
+    public static void main(String[] args) {
+
+    }
+}
