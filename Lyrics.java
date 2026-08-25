6c1e22ef437e56d5163db6922e3831f439ea9769
ULTIMATEFIGHTEER <136517540+ULTIMATEFIGHTEER@users.noreply.github.com>
2026-08-24 20:23:49 -0700

    Update Lyrics.java
    write the program

--- Lyrics.java
+++ Lyrics.java
@@ -2,8 +2,8 @@
 /**
  * Prints out lyrics.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @ULTIMATEFIGHTEER (your name)
+ * @24/8/2026 (a version number or a date)
  */
 public class Lyrics
 {
@@ -13,6 +13,91 @@ public class Lyrics
      * @param args Not used
      **/
     public static void main(String[] args) {
+        lyrics();
+    }
 
+    public static void lyrics() {
+        System.out.println("On a glorious quest");
+        System.out.println("For glory");
+        System.out.println("With his hands of stone");
+        System.out.println("And knees that never buckle");
+        System.out.println("Come and witness the tale");
+        System.out.println("Of the warrior, Knuckles");
+        System.out.println("When our hero was only a lad");
+        System.out.println("He was trained by the greatest, his dad");
+        System.out.println("Had the technique and also the guts");
+        System.out.println("He prepared to kick multiple butts");
+        System.out.println("But the Giant Owls, they did come");
+        System.out.println("They had one mission: to kill everyone");
+        System.out.println("Knuckles and Dad didn't know what to do");
+        System.out.println("Yes, they were doomed");
+        System.out.println("The owls, they clawed and they kicked");
+        System.out.println("They hooted and flew into fits");
+        System.out.println("They set fire to the town");
+        System.out.println("And burned it to the ground");
+        System.out.println("I'm so freaking upset");
+        System.out.println("I can't sing about it");
+        System.out.println("Father, you can't leave me.");
+        System.out.println("I won't, son! I never...");
+        System.out.println("No!");
+        System.out.println("Some things can't be fixed, even with big gloves");
+        System.out.println("Saddest story that you ever heard of");
+        System.out.println("Now Knuckles was sad and alone");
+        System.out.println("Without a father and without a home");
+        System.out.println("(The Giant Owls killed his dad)");
+        System.out.println("So, off on his own, he just needed the power");
+        System.out.println("I will find the Flames of Disaster");
+        System.out.println("(Michael Bolton, everyone)");
+        System.out.println("(A glorious quest for glory)");
+        System.out.println("I need to find the Flames of Disaster");
+        System.out.println("A glorious quest for glory");
+        System.out.println("But the power was no simple task");
+        System.out.println("Too many obstacles lay in his path");
+        System.out.println("Stinking to high heaven, needing a bath");
+        System.out.println("That's when he saw the demon made of fire and ash");
+        System.out.println("Who said...");
+        System.out.println("I'm gonna burn you alive 'till you're crisp");
+        System.out.println("Then boil your bones and eat you like a fish");
+        System.out.println("Then I might hit Facebook Marketplace up");
+        System.out.println("Might be some deals on some freaking cool stuff");
+        System.out.println("But our hero went back to the start");
+        System.out.println("Saw that the strength was deep inside his heart");
+        System.out.println("Yahoo");
+        System.out.println("Yow!");
+        System.out.println("Are you ready? Let's go");
+        System.out.println("But the demon was not impressed");
+        System.out.println("By the puny red guy before him");
+        System.out.println("He looked down and rolled his eyes");
+        System.out.println("And then, without any warning");
+        System.out.println("Oh my God! He used magic and lava");
+        System.out.println("And before you even knew it");
+        System.out.println("Knuckles was defeated, his mission uncompleted");
+        System.out.println("He totally freaking blew it!");
+        System.out.println("Oh my God!");
+        System.out.println("Broken and vanquished, our hero layed on the ground");
+        System.out.println("As the demon screamed out...");
+        System.out.println("You're going down!");
+        System.out.println("Unskilled, untrained, so unworthy");
+        System.out.println("Unskilled, untrained, so unworthy");
+        System.out.println("Unskilled, untrained, so unworthy");
+        System.out.println("You suck");
+        System.out.println("You freaking suck");
+        System.out.println("And now, I'm afraid, this is the end of your story");
+        System.out.println("Is it too late to find out where my strength lies?");
+        System.out.println("I didn't feel it in my bones when I flew high");
+        System.out.println("It wasn't in the lightning coursing off of my skin");
+        System.out.println("Could a warrior's true strength come from within?");
+        System.out.println("(Michael Bolton, everyone)");
+        System.out.println("And just when everyone counted him out");
+        System.out.println("Knuckles rose up with a furious shout");
+        System.out.println("Take heed, demon! We've entered our final fight!");
+        System.out.println("Now prepare for my glorious might!");
+        System.out.println("You fool, only I can be the power's master");
+        System.out.println("No! You're wrong! 'Cause now I wield");
+        System.out.println("The Flames of Disaster!");
+        System.out.println("It wasn't even close when our boy got going");
+        System.out.println("A royal beatdown, with flame fists a-glowing");
+        System.out.println("I've had this power right from the start");
+        System.out.println("But now, I know that true strength lies in a warrior's heart!");
     }
 }
